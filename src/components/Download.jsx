9feaddf2b9cdeffc97af651c3e1bedba9c224be1
Ritik  Kumar <rsengar0089@gmail.com>
2024-07-14 14:35:33 +0530

    updated Download button

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,19 +3,12 @@ import React from 'react';
 
 
 const Download = () => {
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = "../../public/Ritik's Resume.pdf";
-    link.setAttribute('download', "Ritik's Resume");
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-  };
 
   return (
     <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-20'>
       <div className='flex justify-center items-center'>
-       <button onClick={handleDownload} className="bg-red-700 hover:scale-110 duration-100 text-white font-bold px-4 py-2 rounded">Download Resume</button>
+        <a className="bg-red-700 hover:scale-110 duration-100 text-white font-bold px-4 py-2 rounded" 
+        href="../../Ritik's Resume.pdf" download="Ritik's Resume.pdf">Download Resume</a>
       </div>
     </div>
   )
