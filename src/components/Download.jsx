dcbd0f10fc2bf944b9616fc8bfefd00fd0c3e60f
Ritik  Kumar <rsengar0089@gmail.com>
2024-07-14 14:43:26 +0530

    updated Download buttton

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,7 +8,7 @@ const Download = () => {
     <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-20'>
       <div className='flex justify-center items-center'>
         <a className="bg-red-700 hover:scale-110 duration-100 text-white font-bold px-4 py-2 rounded" 
-        href="../../Ritik's Resume.pdf" download="Ritik's Resume.pdf">Download Resume</a>
+        href="/Ritik's Resume.pdf" download="Ritik's Resume.pdf">Download Resume</a>
       </div>
     </div>
   )
